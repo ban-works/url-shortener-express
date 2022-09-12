c8bbd5ef2202d3e93d45f67cfca991cae7fbd4b9
Alban Fresil <alban.fresil@gmail.com>
2022-09-12 15:33:14 +0200

    added http regex

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+
 const URLModel = require("../models/URL");
 
 /* GET home page. */
-router.get('/:suffix', async function(req, res, next) {
-  let suffix = req.params.suffix
-  console.log('suffix', suffix);
-  if(suffix) {
-    try{
-      let requestedURL = await URLModel.findOne({suffix:suffix});
+router.get("/:suffix", async function (req, res, next) {
+  let suffix = req.params.suffix;
+  if (suffix) {
+    try {
+      let requestedURL = await URLModel.findOne({ suffix: suffix });
       if (requestedURL) {
         console.log(requestedURL);
         res.redirect(`${requestedURL.URL}`);
-      }
-      else{
+      } else {
         res.status(500).json("Unknown URL");
-
       }
-
-
-    }
-    catch(error){
+    } catch (error) {
       res.status(500).json(error);
     }
   }
--- routes/shortlink.js
+++ routes/shortlink.js
@@ -1,29 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+require("dotenv").config();
 const URLModel = require("../models/URL");
 const crypto = require("crypto");
-const domain = "http://localhost:3000"
+const domain = "http://localhost:3000";
+const { DOMAIN } = process.env;
 
 /* GET home page. */
-router.post('/', async function(req, res, next) {
-    console.log('body',req.body);
+router.post("/", async function (req, res, next) {
+  var httpRegex =
+    /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+  if (httpRegex.test(req.body.URL)) {
     try {
-        let suffix = crypto.randomBytes(6).toString("hex")
-        console.log(suffix)
-        const newURL = new URLModel({
-          URL: req.body.URL,
-          suffix: suffix ,
-          owner: req.body.owner,
-          dateCreated: Date.now(),
-        });
-        const savedURL = await newURL.save();
-        console.log(savedURL);
-        res.status(200).json(`${domain}/${savedURL.suffix}`);
-      } catch (error) {
-        res.status(500).json(error);
-      }
-
+      let suffix = crypto.randomBytes(6).toString("hex");
+      console.log(suffix);
+      const newURL = new URLModel({
+        URL: req.body.URL,
+        suffix: suffix,
+        owner: req.body.owner,
+        dateCreated: Date.now(),
+      });
+      const savedURL = await newURL.save();
+      res.status(200).json(`${DOMAIN}/${savedURL.suffix}`);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(500).json(`Invalid URL`);
+  }
 });
 
 module.exports = router;
